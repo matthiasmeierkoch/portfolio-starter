fb98e5bd84618050900fb534097101c8bde4dba2
Matthias Koch <matthiasmeierkoch@hotmail.com>
2025-02-22 23:11:21 +0100

    Slightly restructuring the costa rica blog post

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Page() {
         where I focus on creating advertiser exoeriences that connect creators, customers and businesses.
         Before Google, I was designing TV and cloud products at Swisscom and a variety of corporate websites at Internezzo
       </p>
-      
+
     <div className="my-8">
-      <h2 className="mb-8 text-lg font-semibold tracking-tighter">
+      <h2 className="mb-8 text-lg font-semibold tracking-tighter ">
         Projects I've enjoyed working on:
       </h2>
         <ProjectPosts />
