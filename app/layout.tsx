e314acaba96c79f3d4be26b62fcc6acaf12e60a8
Matthias Koch <matthiasmeierkoch@hotmail.com>
2025-04-16 10:52:44 +0200

    removed the @ symbols and added more line hight

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     title: 'Matthias Koch – UX Designer',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Matthias Koch UX Design Portfolio',
     locale: 'en_US',
     type: 'website',
   },
@@ -54,7 +54,7 @@ export default function RootLayout({
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+          <Navbar/>
           {children}
           <Footer />
           <Analytics />
--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,16 @@ export default function Page() {
     <section>
         <h1>User experience and interface designer for digital products based in Switzerland</h1>
         <br></br>
-    <div className="mb-4 text-xl/relaxed tracking-normal">
+    <div className="mb-8 text-xl/relaxed tracking-normal">
       <p>
-        Hello, nice to meet you – I'm Matthias Koch, a user experience and interface designer currently working <a href="https://google.com" target="_blank" className="text-blue-600 visited:text-purple-600 bg-gray-100 dark:bg-gray-900 rounded-xl px-2 py-1">@Google</a> 
+        Hello, nice to meet you – I'm Matthias Koch, a user experience and interface designer currently working at <a href="https://google.com" target="_blank" className="text-blue-600 visited:text-purple-600 bg-gray-100 dark:bg-gray-900 rounded-xl px-2 py-1">Google</a> 
         where I focus on creating advertiser experiences that connect creators, customers and businesses
       </p>
       <p>
-        Before Google, I was designing TV and cloud products <a href="https://www.swisscom.ch/de/privatkunden.html" target="_blank" className="text-blue-600 visited:text-purple-600 bg-gray-100 dark:bg-gray-900 rounded-xl px-2 py-1">@Swisscom</a> and custom web solutions <a href='https://www.internezzo.ch/'  target="_blank" className="text-blue-600 visited:text-purple-600 bg-gray-100 dark:bg-gray-900 rounded-xl px-2 py-1">@Internezzo</a>
+        Before Google, I was designing TV and cloud products at <a href="https://www.swisscom.ch/de/privatkunden.html" target="_blank" className="text-blue-600 visited:text-purple-600 bg-gray-100 dark:bg-gray-900 rounded-xl px-2 py-1">Swisscom</a> and custom web solutions at <a href='https://www.internezzo.ch/'  target="_blank" className="text-blue-600 visited:text-purple-600 bg-gray-100 dark:bg-gray-900 rounded-xl px-2 py-1">Internezzo</a>
       </p>
     </div>
+
     <div className="my-8">
       <h2 className="mb-8 text-lg font-semibold ">
         Projects I've enjoyed working on:
