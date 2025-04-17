5f6b79f9ddc85a8f56510f114951958fbc588652
Matthias Koch <matthiasmeierkoch@hotmail.com>
2025-04-17 15:11:35 +0200

    added routing / slug again for the projects

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,102 @@
+import { notFound } from 'next/navigation'
+import { CustomMDX } from 'app/components/mdx'
+import { formatDate, getProjects } from 'app/projects/utils'
+import { baseUrl } from 'app/sitemap'
+
+export async function generateStaticParams() {
+  let projects = getProjects()
+
+  // Return all project slugs
+  return projects.map((project) => ({
+    slug: project.slug,
+  }))
+}
+
+export function generateMetadata({ params }) {
+  let project = getProjects().find((project) => project.slug === params.slug)
+
+  // Return 404 metadata if the project is not found
+  if (!project) {
+    return
+  }
+
+  let {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+  } = project.metadata
+  let ogImage = image
+    ? image
+    : `${baseUrl}/og?title=${encodeURIComponent(title)}`
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'article',
+      publishedTime,
+      url: `${baseUrl}/projects/${project.slug}`,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [ogImage],
+    },
+  }
+}
+
+export default function Project({ params }) {
+  let project = getProjects().find((project) => project.slug === params.slug)
+
+  // Return 404 if the project is not found
+  if (!project) {
+    notFound()
+  }
+
+  return (
+    <section>
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'CreativeWork',
+            headline: project.metadata.title,
+            datePublished: project.metadata.publishedAt,
+            dateModified: project.metadata.publishedAt,
+            description: project.metadata.summary,
+            image: project.metadata.image
+              ? `${baseUrl}${project.metadata.image}`
+              : `/og?title=${encodeURIComponent(project.metadata.title)}`,
+            url: `${baseUrl}/projects/${project.slug}`,
+            author: {
+              '@type': 'Person',
+              name: 'My Portfolio',
+            },
+          }),
+        }}
+      />
+      <h1 className="title font-semibold text-2xl tracking-tighter">
+        {project.metadata.title}
+      </h1>
+      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          {formatDate(project.metadata.publishedAt)}
+        </p>
+      </div>
+      <article className="prose">
+        <CustomMDX source={project.content} />
+      </article>
+    </section>
+  )
+}
